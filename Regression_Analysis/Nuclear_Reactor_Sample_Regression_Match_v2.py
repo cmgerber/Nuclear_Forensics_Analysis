af5dddf5337b2f53d21645dcc5e28855405353d0
Colin Gerber <colin.gerber@gmail.com>
2014-08-03 13:17:01 -0700

    add batch unknown sample processing

--- Regression_Analysis/Nuclear_Reactor_Sample_Regression_Match_v2.py
+++ Regression_Analysis/Nuclear_Reactor_Sample_Regression_Match_v2.py
@@ -5,6 +5,7 @@ __python_version__ = '2.7.5'
 
 import sys
 import math
+import os
 from copy import deepcopy
 from scipy.optimize import fmin
 import numpy as np
@@ -19,31 +20,29 @@ def import_data():
     '''This function takes in data from a .csv and stores it in a pandas
     dataframe. The file must be a .xls or xls.'''
 
-    while (True):
-        try:
-            file_name = raw_input('What is the excel file name you would like to use? ')
-            data = pd.read_excel(file_name, 'Sheet1')
-            test_case = pd.read_excel(file_name, 'Sheet2')
-            break
-        except Exception as e:
-            print e
-            print 'The file you entered does not exist in the directory.'
-            file_path = raw_input('Please enter the path to your file (or enter "try" to retype the file name): ')
-            if file_path.lower() == 'try':
-                pass
-            else:
-                os.chdir(file_path)
+    # while (True):
+    #     try:
+    #         file_name = raw_input('What is the excel file name you would like to use? ')
+    #         data = pd.read_excel(file_name, 'Sheet1')
+    #         test_case = pd.read_excel(file_name, 'Sheet2')
+    #         break
+    #     except Exception as e:
+    #         print e
+    #         print 'The file you entered does not exist in the directory.'
+    #         file_path = raw_input('Please enter the path to your file (or enter "try" to retype the file name): ')
+    #         if file_path.lower() == 'try':
+    #             pass
+    #         else:
+    #             os.chdir(file_path)
 
     #use for easy testing.
-    # data = pd.read_excel('VVER_RBMK_BWR_enrichment_generated.xlsx', 'Sheet1')
-    # test_case = pd.read_excel('VVER_RBMK_BWR_enrichment_generated.xlsx', 'Sheet2')
-    # return data, test_case
+    data = pd.read_excel('VVER_RBMK_BWR_enrichment_generated.xlsx', 'Sheet1')
+    test_case = pd.read_excel('VVER_RBMK_BWR_enrichment_generated.xlsx', 'Sheet2')
+    return data, test_case
 
 def run_analysis(data, base_column, ltitles, test_case):
     
-    regression_dist_dict, closest_curve_dict, reactors = regression(data, base_column,ltitles, test_case)
-
-    create_output(regression_dist_dict, closest_curve_dict, reactors)
+    regression(data, base_column,ltitles, test_case)
 
 def regression(data, base_column,ltitles, test_case):
     '''This function does a polynomial regression on each of the reactor ratio pairs
@@ -55,131 +54,142 @@ def regression(data, base_column,ltitles, test_case):
     regression_dist_dict = {}
     closest_curve_dict = {}
     react_len = 0
+
+    #counter for new file name for each sample
+    name_counter = 0
     
-    #iterate through each ratio and compare it to the baseline ratio
-    for current_name in ltitles:
-        color = get_color()
+    #iterate through each unknown sample
+    for ix, unknown in test_case.iterrows():
+        #update name counter for next sample
+        name_counter += 1
 
-        #set min distance variable for keeping track of the closest curve
-        min_dist = ('temp', 'temp', 1000)
+        #iterate through each ratio and compare it to the baseline ratio
+        for current_name in ltitles:
+            color = get_color()
 
-        #initiate plot
-        fig = plt.figure(figsize=(10,6))
-        ax  = fig.add_subplot(111)
+            #set min distance variable for keeping track of the closest curve
+            min_dist = ('temp', 'temp', 1000)
 
-        for i, group in data.groupby(['reactor', 'enrichment']):
-            #creates list of reactors that have been iterated through
-            if i[0] not in reactor_name:
-                reactor_name.append(i[0])
+            #initiate plot
+            fig = plt.figure(figsize=(10,6))
+            ax  = fig.add_subplot(111)
 
-            #only calls new color when the iteration reaches a new reactor
-            if react_len != len(reactor_name):
-                #calls next color to be used in graphing. One for each reactor.
-                new_color = next(color)
-            react_len = len(reactor_name)
+            for i, group in data.groupby(['reactor', 'enrichment']):
+                #creates list of reactors that have been iterated through
+                if i[0] not in reactor_name:
+                    reactor_name.append(i[0])
 
-            print '\n \n \n Reactor: ', i[0]
-            print 'Enrichment: ', i[1]
+                #only calls new color when the iteration reaches a new reactor
+                if react_len != len(reactor_name):
+                    #calls next color to be used in graphing. One for each reactor.
+                    new_color = next(color)
+                react_len = len(reactor_name)
 
-            x = list(group[base_column])
-            y = list(group[current_name])
-            unknown_sample = [test_case[base_column], test_case[current_name]]
+                print '\n \n \n Reactor: ', i[0]
+                print 'Enrichment: ', i[1]
 
-            #this checks if there is a (0,0) point in the data, if there is not one
-            #it will append one to the data so that the regression lines have a y intercept
-            #of 0.
-            if 0 not in x and 0 not in y:
-                x.append(0)
-                y.append(0)
-                print 'appended'
-            
+                x = list(group[base_column])
+                y = list(group[current_name])
+                unknown_sample = [unknown[base_column], unknown[current_name]]
 
-            #used to draw a connecting line line from the uknown point to the curve.
-            coords = zip(x,y)
-            line = geom.LineString(coords)
-            unknown_samples = zip(list(test_case[base_column]),list(test_case[current_name]))
-            point = geom.Point(unknown_samples)
-            
-            print 'Uknown sample: ', unknown_samples                            
+                #this checks if there is a (0,0) point in the data, if there is not one
+                #it will append one to the data so that the regression lines have a y intercept
+                #of 0.
+                if 0 not in x and 0 not in y:
+                    x.append(0)
+                    y.append(0)
+                
 
-            #creates the regression line
-            p = np.poly1d(np.polyfit(x,y, 2))            
-            print '%s enrich: %s: %s vs. %s ' % (i[0], i[1], current_name, base_column), p
-            
-            #using the coefficients and unknown value calculates the distance from the regression
-            #line to the unknown point.
-            px = unknown_samples[0][0]; py = unknown_samples[0][1]; a = p.c[0]; b = p.c[1]; c = p.c[2]
-            print px,py, a, b, c
-            print type(a)
+                #used to draw a connecting line line from the uknown point to the curve.
+                coords = zip(x,y)
+                line = geom.LineString(coords)
+                unknown_samples = (unknown[base_column],unknown[current_name])
+                point = geom.Point(unknown_samples)
+                
+                print 'Uknown sample: ', unknown_samples                            
 
-            #finds the minimum value of the distance function
-            funct_min_x = fmin(my_distance_formula, 0,args=(px,py,a,b,c))
-            d = my_distance_formula(funct_min_x[0],px,py,a,b,c)
-            print 'distance from %s enrich: %s:' % (i[0], i[1]), d, '\n \n \n'
+                #creates the regression line
+                p = np.poly1d(np.polyfit(x,y, 2))            
+                print '%s enrich: %s: %s vs. %s ' % (i[0], i[1], current_name, base_column), p
+                
+                #using the coefficients and unknown value calculates the distance from the regression
+                #line to the unknown point.
+                px = unknown_samples[0]; py = unknown_samples[1]; a = p.c[0]; b = p.c[1]; c = p.c[2]
+                print px,py, a, b, c
+                print type(a)
 
-            #finds the y value of the min point on the curve
-            funct_min_y = my_curve(funct_min_x, a, b, c)
+                #finds the minimum value of the distance function
+                funct_min_x = fmin(my_distance_formula, 0,args=(px,py,a,b,c))
+                d = my_distance_formula(funct_min_x[0],px,py,a,b,c)
+                print 'distance from %s enrich: %s:' % (i[0], i[1]), d, '\n \n \n'
 
-            #creates a dictionary containing the distance from the unknown point to all the regression lines
-            if current_name not in regression_dist_dict:regression_dist_dict[current_name] = []
-            regression_dist_dict[current_name].append([i[0], i[1], d])
+                #finds the y value of the min point on the curve
+                funct_min_y = my_curve(funct_min_x, a, b, c)
 
-            
-            #this creates a graph for each regression
-            #change axis range with np.linespace below
-            xp = np.linspace(0, 1.2, 100)
-            ax.plot(x, y, '.', c = new_color, label = '%s: %s' %(i[0], i[1]))
-            ax.plot(xp, p(xp), '-', c = new_color)
-            plt.ylim(-.1,.5)
-            plt.ylabel(current_name); plt.xlabel(base_column)
+                #creates a dictionary containing the distance from the unknown point to all the regression lines
+                if current_name not in regression_dist_dict:regression_dist_dict[current_name] = []
+                regression_dist_dict[current_name].append([i[0], i[1], d])
 
-            #finds the closest curve for the current pu ratios and
-            #then plots the unknown point with a line connecting it to the curve.
-            for ratio in regression_dist_dict[current_name]:
-                if ratio[2] < min_dist[2]: 
-                    min_dist = ratio
-                    plot_line = line
-                    min_point = [funct_min_x, funct_min_y]
-                    closest_curve_values = [i[0], i[1] ,d]
-                    line_equation = p
+                
+                #this creates a graph for each regression
+                #change axis range with np.linespace below
+                xp = np.linspace(0, 1.2, 100)
+                ax.plot(x, y, '.', c = new_color, label = '%s: %s' %(i[0], i[1]))
+                ax.plot(xp, p(xp), '-', c = new_color)
+                plt.ylim(-.1,.5)
+                plt.ylabel(current_name); plt.xlabel(base_column)
 
-        print 'MIN_DIST: ', min_dist
-        print 'CURRENT NAME', current_name
-        print unknown_sample, min_point
+                #finds the closest curve for the current pu ratios and
+                #then plots the unknown point with a line connecting it to the curve.
+                for ratio in regression_dist_dict[current_name]:
+                    if ratio[2] < min_dist[2]: 
+                        min_dist = ratio
+                        plot_line = line
+                        min_point = [funct_min_x, funct_min_y]
+                        closest_curve_values = [i[0], i[1] ,d]
+                        line_equation = p
 
-        closest_curve_dict[current_name] = closest_curve_values
+            print 'MIN_DIST: ', min_dist
+            print 'CURRENT NAME', current_name
 
-        #plots the unknown point and a line connecting it to the curve.
-        # ax.plot([unknown_sample[0], min_point[0]], [unknown_sample[1],min_point[1]], color= next(color), marker='o', label = 'Uknown Sample1' )
-        # point_on_line = plot_line.interpolate(plot_line.project(point))
-        # ax.plot([point.x, point_on_line.x], [point.y, point_on_line.y], color= next(color), marker='o', label = 'Uknown Sample')
 
-        #plots the unknown sample
-        ax.plot(unknown_sample[0], unknown_sample[1], color= next(color), marker='o', label = 'Uknown Sample')
+            closest_curve_dict[current_name] = closest_curve_values
 
-        #plots the closest curve and highlights it by having it red and thicker.
-        ax.plot(xp, line_equation(xp), '-', c = 'r', lw=3.0, label='%s: %s - closest' %(closest_curve_values[0], closest_curve_values[1]))
+            #plots the unknown point and a line connecting it to the curve.
+            # ax.plot([unknown_sample[0], min_point[0]], [unknown_sample[1],min_point[1]], color= next(color), marker='o', label = 'Uknown Sample1' )
+            # point_on_line = plot_line.interpolate(plot_line.project(point))
+            # ax.plot([point.x, point_on_line.x], [point.y, point_on_line.y], color= next(color), marker='o', label = 'Uknown Sample')
 
-        plt.title('%s: %s vs. %s' % (', '.join(map(str, reactor_name)), current_name, base_column))
-        ax.set_position([0.1,0.1,0.5,0.8])
-        ax.legend(loc = 'center left', bbox_to_anchor = (1.0, 0.5))
-        #saves the current plot to a temp variable so it can save plot to file after show()
-        temp_plot = plt.gcf()
+            #plots the unknown sample
+            ax.plot(unknown_sample[0], unknown_sample[1], color= next(color), marker='o', label = 'Uknown Sample')
 
-        #comment below in if you want each graph to pop up when you run the script
-        # plt.show()
+            #plots the closest curve and highlights it by having it red and thicker.
+            ax.plot(xp, line_equation(xp), '-', c = 'r', lw=3.0, label='%s: %s - closest' %(closest_curve_values[0], closest_curve_values[1]))
 
-        saveitem = current_name.replace('/', '')
-        savebase_column = base_column.replace('/', '')
-        #saves plot to .png file
-        temp_plot.savefig('%s%s%s_regression.png' % (''.join(map(str, reactor_name)), saveitem, savebase_column))
+            plt.title('%s: %s vs. %s' % (', '.join(map(str, reactor_name)), current_name, base_column))
+            ax.set_position([0.1,0.1,0.5,0.8])
+            ax.legend(loc = 'center left', bbox_to_anchor = (1.0, 0.5))
+            #saves the current plot to a temp variable so it can save plot to file after show()
+            temp_plot = plt.gcf()
 
-        #saving the reactor list for naming of the output
-        reactors = deepcopy(reactor_name)
-        del reactor_name[:]
-    print 'dict', regression_dist_dict
-    print 'closest', closest_curve_dict
-    return regression_dist_dict, closest_curve_dict, reactors
+            #comment below in if you want each graph to pop up when you run the script
+            # plt.show()
+
+            #create file name addtion
+            name_add = 'sample_%d' %(name_counter)
+
+            saveitem = current_name.replace('/', '')
+            savebase_column = base_column.replace('/', '')
+            #saves plot to .png file
+            temp_plot.savefig('%s%s%s_regression_%s.png' % (''.join(map(str, reactor_name)), saveitem, savebase_column, name_add))
+
+            #saving the reactor list for naming of the output
+            reactors = deepcopy(reactor_name)
+            del reactor_name[:]
+        print 'dict', regression_dist_dict
+        print 'closest', closest_curve_dict
+        create_output(regression_dist_dict, closest_curve_dict, reactors, name_add)
+
 
 def my_curve(x, a, b, c):
     return (a*x)**2 + (b*x) + c
@@ -196,7 +206,7 @@ def get_color():
     for color in ['b', 'g', 'y', 'm', 'c', 'k']:
         yield color  
 
-def create_output(regression_dist_dict, closest_curve_dict, reactor_name): 
+def create_output(regression_dist_dict, closest_curve_dict, reactor_name,name_add): 
     '''Converts the dictionaries into dataframes to format for saving as
     an excel. The total resutls on the first sheet and closest curves on the second'''
 
@@ -216,7 +226,7 @@ def create_output(regression_dist_dict, closest_curve_dict, reactor_name):
     print 'total_results', total_results
     print 'closest_results', closest_results
     print reactor_name
-    file_name = '%s_regression_results.xlsx' %('_'.join(map(str, reactor_name)))
+    file_name = '%s_regression_results_%s.xlsx' %('_'.join(map(str, reactor_name)), name_add)
 
     writer = ExcelWriter(file_name)
 
